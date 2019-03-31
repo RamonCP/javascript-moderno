834f3be285a97f276c128ba8316f766163020b29
RamonCP <ramoncavpires@gmail.com>
2019-03-30 23:00:53 -0300

    Object Destructuring

---
+++ object-destructuring/index.js
@@ -0,0 +1,15 @@
+const developer = {
+    id: 1,
+    user: 'RamonCP',
+    age: '18',
+    learning: 'JavaScript'
+}
+
+console.log(developer.learning);
+
+const { id, user, age, learning } = developer;
+
+console.log(id)
+console.log(user)
+console.log(age)
+console.log(learning)
